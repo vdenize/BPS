bd1f074687d75a52a3fc4e29c9726ad23b8d739c
vdenize <denizeev17@gmail.com>
2018-08-25 02:47:48 +0800

    Login Page

--- login/urls.py
+++ login/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'login'
 urlpatterns = [
     path('', views.index, name='index'),
     path('signup/', views.sign_up, name='signup')
