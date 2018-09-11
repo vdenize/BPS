4e811d3266eff2a8014b8c0fc33324d3babbe0d3
vdenize <denizeev17@gmail.com>
2018-09-11 20:42:41 +0800

    Refresh Calendar with Data

--- reservation/views.py
+++ reservation/views.py
@@ -1,7 +1,11 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
 
 # Create your views here.
+from reservation.models import Events
+
+
 def index(request):
     context = {
 
@@ -21,3 +25,57 @@ def calendar(request):
 
     }
     return render(request, 'calendar/calendar.html', context)
+
+
+def adding_event(request):
+    title = request.GET.get('title', None)
+    start = request.GET.get('start', None)
+    end = request.GET.get('end', None)
+    event = Events(
+        title=str(title),
+        start=str(start),
+        end=str(end)
+    )
+    event.save()
+    data = {
+        "title": title,
+        "start": start,
+        "end": end
+             }
+    return JsonResponse(data)
+
+
+def deleting_event(request):
+    title = request.GET.get('title', None)
+    start = request.GET.get('start', None)
+    end = request.GET.get('end', None)
+    event = Events.objects.get(title=title)
+    event.delete()
+    data = {
+        "title": title,
+        "start": start,
+        "end": end
+             }
+    return JsonResponse(data)
+
+
+def get_events(request):
+    # title_list = []
+    # start_list = []
+    # end_list = []
+    events = Events.objects.all()
+    for each in events:
+        # title_list.append(each.title)
+        # start_list.append(each.start)
+        # end_list.append(each.end)
+        # data = {
+        #     "titles": title_list,
+        #     "starts": start_list,
+        #     "ends": end_list,
+        # }
+    data = {
+            "titles": each.title,
+            "starts": each.start,
+            "ends": each.end,
+        }
+    return JsonResponse(data)
