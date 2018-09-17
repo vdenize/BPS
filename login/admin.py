b501d3cb754a1dda36fba740a775e4303f452726
Scurraaa <bacaijoshuaa@gmail.com>
2018-09-18 02:31:13 +0800

    Unfinished Calendar

--- login/admin.py
+++ login/admin.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 # Register your models here.
 from login.models import Profile, User_File
 
-admin.site.regiter(Profile)
-admin.site.regiter(User_File)
+admin.site.register(Profile)
+admin.site.register(User_File)
