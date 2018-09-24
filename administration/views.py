52dc17ff0d9937900228210cbec4a1ece1476dc0
XOR083199 <sorianodanielb99@gmail.com>
2018-09-25 00:43:28 +0800

    Dashboard Admin Fixed

--- administration/views.py
+++ administration/views.py
@@ -1,12 +1,17 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
 
 # Create your views here.
+from reservation.models import Event
+
+
 def index(request):
     context = {
 
     }
-    return render(request, 'admin/index.html', context)
+    return render(request, 'admin/main.html', context)
+
 
 def calendar(request):
     context ={
